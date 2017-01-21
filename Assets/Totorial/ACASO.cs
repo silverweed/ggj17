ce812d91d003637cd621589c7a19c90572534006
Francesco Komauli <francesco.komauli@gmail.com>
2017-01-21 14:24:59 +0100

    Totorial pad showing game commands

---
+++ Assets/Totorial/ACASO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ACASO : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject.FindObjectOfType<TotorialPad>().ShowSquare();	
+	}
+}
---
+++ Assets/Totorial/TotorialPad.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class TotorialPad : MonoBehaviour {
+
+    GameObject pad;
+    GameObject leftHorizontal;
+    GameObject leftVertical;
+    GameObject sin;
+    GameObject square;
+    GameObject sawtooth;
+    GameObject triangle;
+
+    void Awake() {
+        pad = transform.FindChild("Pad").gameObject;
+        leftHorizontal = transform.FindChild("LeftHorizontal").gameObject;
+        leftVertical = transform.FindChild("LeftVertical").gameObject;
+        sin = transform.FindChild("Sin").gameObject;
+        square = transform.FindChild("Square").gameObject;
+        sawtooth = transform.FindChild("Sawtooth").gameObject;
+        triangle = transform.FindChild("Triangle").gameObject;
+        pad.SetActive(false);
+        foreach (Transform child in transform) { child.gameObject.SetActive(false); }
+    }
+
+    public void ShowLeftHorizontal() {
+        pad.SetActive(true);
+        leftHorizontal.SetActive(true);
+    }
+
+    public void ShowLeftVertical() {
+        pad.SetActive(true);
+        leftVertical.SetActive(true);
+    }
+
+    public void ShowSin() {
+        pad.SetActive(true);
+        sin.SetActive(true);
+    }
+
+    public void ShowSquare() {
+        pad.SetActive(true);
+        square.SetActive(true);
+    }
+
+    public void ShowSawtooth() {
+        pad.SetActive(true);
+        sawtooth.SetActive(true);
+    }
+
+    public void ShowTriangle() {
+        pad.SetActive(true);
+        triangle.SetActive(true);
+    }
+
+    public void Hide() {
+        foreach (Transform child in transform) { child.gameObject.SetActive(false); }
+        pad.SetActive(false);
+    }
+}
