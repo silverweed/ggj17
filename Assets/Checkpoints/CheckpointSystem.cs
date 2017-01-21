b07553eca4a865504e1b759f17b3f4610c0da8b7
Francesco Komauli <francesco.komauli@gmail.com>
2017-01-21 14:54:09 +0100

    Setting a default first checkpoint at the level beginning

--- Assets/Checkpoints/CheckpointSystem.cs
+++ Assets/Checkpoints/CheckpointSystem.cs
@@ -8,11 +8,23 @@ public class CheckpointSystem : MonoBehaviour {
     List<Checkpoint> checkpoints = new List<Checkpoint>();
     bool firstCheckpointReached = false;
 
+    Wave.Shape initialShape;
+    float initialAmplitude;
+    float initialFrequency;
+    float initialPhase;
+    float initialOffset;
+
     void Awake() {
         trackedObject = GameObject.FindObjectOfType<Electron>();
         wave = GameObject.FindObjectOfType<Wave>();
         foreach (Transform child in transform) { checkpoints.Add(child.gameObject.GetComponent<Checkpoint>()); }
         checkpoints.Sort(CompareByAxisX);
+
+        initialShape = wave.shape;
+        initialAmplitude = wave.amplitude;
+        initialFrequency = wave.frequency;
+        initialPhase = wave.phase;
+        initialOffset = wave.offset;
     }
 
     static int CompareByAxisX(Checkpoint a, Checkpoint b) {
@@ -52,8 +64,11 @@ public class CheckpointSystem : MonoBehaviour {
 
     public void MoveToLastCheckpoint() {
         if (!firstCheckpointReached) {
-            wave.phase = 0;
-            wave.offset = 0;
+            wave.phase = initialPhase;
+            wave.offset = initialOffset;
+            wave.shape = initialShape;
+            wave.amplitude = initialAmplitude;
+            wave.frequency = initialFrequency;
             Camera.main.GetComponent<AudioSource>().time = 0f;
         } else {
             SyncToWave(checkpoints[0]);
