813d3cf34e57b6b8c75fd55b6e8a5d9601c8969b
Francesco Komauli <francesco.komauli@gmail.com>
2017-01-21 02:15:56 +0100

    Speed in whatsoever coordinate system

---
+++ Assets/DatSpid.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// How to speed.
+/// </summary>
+public class DatSpid : MonoBehaviour {
+
+    Wave wave;
+
+    void Start() {
+        wave = GameObject.FindObjectOfType<Wave>();
+    }
+
+	void Update () {
+        transform.Translate(-wave.speed * Proportion() * Time.deltaTime, 0f, 0f);
+	}
+
+    float Proportion() {
+        var left = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        var right = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0));
+        return (right.x - left.x) / Screen.width;
+    }
+}
